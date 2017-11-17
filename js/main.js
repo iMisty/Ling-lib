813bf127660c0a749e087c91ccac88b99964beb6
JingYi <qianwumeng_2cyndi@qq.com>
2017-11-17 17:58:38 +0800

    TODO

--- js/main.js
+++ js/main.js
@@ -2,8 +2,13 @@
 $(document).ready(
     $.getScript(
         // TODO
+        // Get data.js and innerHTML to id text
     ),
     $("#button").click(function () {
         $("#nav-bar").slideToggle(400);
     }),
+    $("#jump").click(function(){
+        // TODO
+        // Jump button to section
+    })
 );
