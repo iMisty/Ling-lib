0880ea6c6e796b590eed9054b248cd0f96ec071b
Miya <qianwumeng_2cyndi@qq.com>
2020-04-25 23:10:54 +0800

    NekoMimi

---
+++ NekoMimi-Vue2.6/src/components/text.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>
+        <slot></slot>
+    </p>
+</template>
+<style lang="less" scoped>
+p{
+    margin: 2.6rem;
+    padding-bottom: 1.2rem;
+    letter-spacing: 1px;
+}
+</style>
---
+++ NekoMimi-Vue2.6/src/components/title.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+        <h2>
+          <slot></slot>
+        </h2>
+    </header>
+</template>
+<style lang="less" scoped>
+    header{
+        margin-top: 2.4rem;
+    }
+</style>
